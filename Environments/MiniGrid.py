664fd3fdab5403e4a655e39bb3d4353471de49f2
MatthewBlazevic398 <mblaz398@mtroyal.ca>
2026-02-03 18:38:52 -0700

    Added curriculum learning.

--- Environments/MiniGrid.py
+++ Environments/MiniGrid.py
@@ -2,29 +2,25 @@ import minigrid
 import numpy as np
 import gymnasium as gym
 from minigrid.wrappers import FlatObsWrapper
-from Agents.Agents import Agent
-  
-def main() -> None:
-    env = gym.make("MiniGrid-DoorKey-6x6-v0")
-    env = FlatObsWrapper(env)
-    state_size = env.observation_space.shape[0]
-    action_size = env.action_space.n
+from Agents.Agents import Agent, ReplayMemory
 
-    agent = Agent(
-        state_size=state_size,
-        action_size=action_size,
-        epsilon_decay=0.999
-    )
+def make_env_with_flattening(env_id, render_mode=None):
+    env = gym.make(env_id, render_mode)
+    env = FlatObsWrapper(env)
     
-    num_episodes = 1000
-    target_update_every = 10
-       
+    return env
+
+def training_loop(agent: Agent, env_name, num_episodes = 1000, target_update_every = 10):    
+    print("Training on: ", env_name)
+    env = make_env_with_flattening(env_name)
+    
     for episode in range(num_episodes):
         obs, _ = env.reset()
         state = obs.astype(np.float32)
 
         done = False
         total_reward = 0.0
+        avg_over_update = 0.0
 
         while not done:
             action = agent.get_action(state)
@@ -37,14 +33,40 @@ def main() -> None:
 
             state = next_state
             total_reward += reward
+            avg_over_update += reward
 
         agent.decay_epsilon()
 
         if episode % target_update_every == 0:
             agent.update_target()
             print(f"Episode {episode:4d} | Reward {total_reward:6.2f} | Epsilon {agent.epsilon:.3f}")
+
+def curriculum_learning():
+    temp_env = make_env_with_flattening("MiniGrid-DoorKey-6x6-v0")
+    state_size = temp_env.observation_space.shape[0]
+    action_size = temp_env.action_space.n
+    temp_env.close()
+
+    agent = Agent(
+        state_size=state_size,
+        action_size=action_size,
+        epsilon_decay=0.999
+    )    
+       
+    training_loop(
+        agent=agent,
+        env_name="MiniGrid-Fetch-6x6-N2-v0",
+        num_episodes=1000,
+        target_update_every=10
+    )
+    agent.epsilon = 1.0
     
-    env.close()
+    training_loop(
+        agent=agent,
+        env_name="MiniGrid-DoorKey-6x6-v0",
+        num_episodes=2500,
+        target_update_every=10
+    )
     
 if __name__ == '__main__':
-    main()
+    curriculum_learning()
